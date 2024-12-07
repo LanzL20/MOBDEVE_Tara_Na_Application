5e5a739c17600ff3912dffc7128ed015e7fcda09
Lanz Kendall Y. Lim <114926584+LanzL20@users.noreply.github.com>
2024-12-07 17:41:47 +0800

    Initial google maps implemented

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,10 +5,12 @@ plugins {
     id("com.google.gms.google-services") version "4.4.2" apply false
     id("com.google.firebase.crashlytics") version "3.0.2" apply false
     id("com.google.devtools.ksp") version "1.9.22-1.0.17" apply false
+    alias(libs.plugins.google.android.libraries.mapsplatform.secrets.gradle.plugin) apply false
 }
 
 buildscript {
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.21")
+        classpath("com.google.android.libraries.mapsplatform.secrets-gradle-plugin:secrets-gradle-plugin:2.0.1")
     }
 }
